2ee876692175535667a4d2546edf8b4b3cf721d4
kai <kailashjat604@gmail.com>
2023-02-18 11:36:02 +0530

    Create Home component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
+import Home from "./components/Home";
+
 export default function() {
 	return (
-		<h1>Hello World</h1>
+		<div>
+			<Home/>
+		</div>
 	)
 }
---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+import "./Home.css";
+
+export default function Home() {
+    return (
+        <div className="start-container">
+            <h1 className="title">Quizzical</h1>
+            <p className="desc">Some description if needed</p>
+            <button className="start-btn">Start Quiz</button>
+        </div>
+    )
+}
