29842d9545303a215fc6dcb88b8d752a7b18de2a
kai <kailashjat604@gmail.com>
2023-02-19 13:35:52 +0530

    Process data and store in state

--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -3,18 +3,33 @@ import React from "react";
 
 export default function Questions(props) {
     const {apiUrl, setData} = props;
-    console.log(apiUrl);
 
     React.useEffect(() => {
 		async function processData() {
             const data = (await fetchData(apiUrl)).results;
-            data.map((question) => {
+
+            const questionsData = data.map((question) => {
                 const correctAns = question.correct_answer;
+
                 let options = question.incorrect_answers;
                 options.push(correctAns);
                 options = shuffleArray(options);
+
+                options = options.map(option => ({
+                    value: option,
+                    selected: false,
+                    isCorrect: option === correctAns
+                }));
+
+                return {
+                    questionText: question.question,
+                    options: options,
+                };
             });
+
+            setData(questionsData);
         }
+
         processData();
 	}, [apiUrl]);
 
